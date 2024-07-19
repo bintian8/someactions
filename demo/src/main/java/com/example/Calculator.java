4f86884e1f42621fa5987cd2c4f82ed9ea9e4606
bintian8 <bintian.yanyan8@icloud.com>
2024-07-19 16:19:08 +0900

    test

--- demo/src/main/java/com/example/Calculator.java
+++ demo/src/main/java/com/example/Calculator.java
@@ -13,7 +13,7 @@ public class Calculator {
         return a * b;
     }
 
-    public double divide(int a, int b) {
+    public int divide(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Division by zero is not allowed.");
         }
--- demo/src/test/java/com/example/AppTest.java
+++ demo/src/test/java/com/example/AppTest.java
@@ -40,20 +40,20 @@ public class AppTest
         assertEquals(40, result);
     }
 
-    // @Test
-    // public void testDivide() {
-    //     Calculator calculator = new Calculator();
-    //     double result = calculator.divide(10, 4);
-    //     assertEquals(2.5, result, 0.0001); // 10 / 4 は 2.5 です。第3引数は許容誤差
-    // }
-
     @Test
-    public void testDivide_DivisionByZero_ShouldThrowIllegalArgumentException() {
+    public void testDivide() {
         Calculator calculator = new Calculator();
-        // 例外がスローされることを確認
-        assertThrows(IllegalArgumentException.class, () -> {
-            calculator.divide(10, 0);
-        });
+        int result = calculator.divide(10, 4);
+        assertEquals(2, result); // 10 / 4 は 2.5 です。第3引数は許容誤差
     }
+
+    // @Test
+    // public void testDivide_DivisionByZero_ShouldThrowIllegalArgumentException() {
+    //     Calculator calculator = new Calculator();
+    //     // 例外がスローされることを確認
+    //     assertThrows(IllegalArgumentException.class, () -> {
+    //         calculator.divide(10, 0);
+    //     });
+    // }
 
 }
