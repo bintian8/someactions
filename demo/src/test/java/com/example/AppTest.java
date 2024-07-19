9839e260e7a8337640207d5d1ff0219357e16781
bintian8 <bintian.yanyan8@icloud.com>
2024-07-19 15:31:39 +0900

    test commit

---
+++ demo/src/test/java/com/example/AppTest.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+        @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(3, 4);
+        assertEquals(7, result);
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        int result = calculator.subtract(10, 4);
+        assertEquals(6, result);
+    }
+
+}
